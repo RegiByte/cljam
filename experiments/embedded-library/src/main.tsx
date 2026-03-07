ce82fd10922a8d70dab4ecdba9be6ad087f3634a
Reginaldo Junior <reginaldo.junior@ae.studio>
2026-03-07 10:54:51 -0300

    feat: create proper readme

---
+++ experiments/embedded-library/src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createSession, printString } from 'conjure-js/src/core'
+
+const session = createSession({
+  output: (text) => console.log(text),
+  sourceRoots: ['src/clojure'],
+  readFile: (path) => import.meta.resolve(path),
+})
+
+const result = session.evaluate('(map inc [1 2 3])')
+console.log(printString(result)) // (2 3 4)
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
